f55f4de56e357056406ad76ac7d06da617829d35
Md Hefzul Bari <ratul14april@gmail.com>
2021-12-16 00:40:40 -0600

    Fetching tweets from database & creating posts

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,29 +1,22 @@
-$(document).ready(() => {
-
-$.ajax({
-  url: 'http://localhost:8080/tweets',
-  method: 'GET'
-})
-.then((response)=> {
-  response.forEach((user) => {
-    $("#old-tweet").append(`
+const createTweetElement = (tweet) => {
+  const newTweet = $("#old-tweet").append(`
     <div class="previous-tweets">
       <section class="profile-identifier">
         <div class="avatar-fullname">
-          <img class="avatar" src="${user.user.avatars}">
-          <h2 class="name">${user.user.name}</h2>
+          <img class="avatar" src="${tweet.user.avatars}">
+          <h2 class="name">${tweet.user.name}</h2>
         </div>
-        <h3 class="profile-name">${user.user.handle}</h3>
+        <h3 class="profile-name">${tweet.user.handle}</h3>
       </section>
       <section>
         <div class="create-tweeter-post">
         <div class="prev-tweets">
-          <h2>${user.content.text}</h2></div>
+          <h2>${tweet.content.text}</h2></div>
           <hr>
         </div>
         <div class="tweet-btn-counter">
           <span class="time-till-tweet">
-            <p>${timeago.format(user.created_at)}</p>
+            <p>${timeago.format(tweet.created_at)}</p>
           </span>
           <span class="react-icons">
             <i class="fas fa-flag"></i>
@@ -32,26 +25,41 @@ $.ajax({
           </span>
         </div>
       </section>
-    </div>`);
+    </div>      
+  `);
+  return newTweet;
+};
+
+const renderTweets = (tweetData) => {
+  $("#old-tweet").empty();
+  tweetData.forEach((tweet) => {
+    $("#old-tweet").append(createTweetElement(tweet));
   })
-})
+};
 
-// const $button = $(".tweeter-btn");
-// $button.submit((event) => {
-//   event.preventDefault();
-//   console.log('hello')
-// })
-// $.ajax({
-//   type: "POST",
-//   url: "index.html"
-// })
+$(document).ready(() => {
+  fetchTweets();
+  $("#tweet-form").submit((event) => {
+    event.preventDefault();
 
+    const tweetData = $(this).serialize();
+    $.ajax({
+      type: "POST",
+      url: "http://localhost:8080/tweets/",
+      data: tweetData,
+      success: () => {
+        fetchTweets();
+      },
+    });
+  });
 });
 
-// $.ajax({
-//   type: "POST",
-//   url: "process.php",
-//   data: formData,
-//   dataType: "json",
-//   encode: true,
-// })
+const fetchTweets = () => {
+  $.ajax({
+    type: "GET",
+    url: "http://localhost:8080/tweets/",
+    success: (data) => {
+      renderTweets(data);
+    },
+  });
+};
