430c786f1f312ca6bcc10ad4f07309dfd8c971c3
Md Hefzul Bari <ratul14april@gmail.com>
2021-12-17 00:25:46 -0600

    Added new tweet show/hide, new screen size css

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,17 +1,24 @@
+const escape = (str) => {
+  let div = document.createElement("h2");
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+};
+
 const createTweetElement = (tweet) => {
   const newTweet = $("#old-tweet").append(`
     <div class="previous-tweets">
       <section class="profile-identifier">
         <div class="avatar-fullname">
           <img class="avatar" src="${tweet.user.avatars}">
-          <h2 class="name">${tweet.user.name}</h2>
+          <h5 class="name">${tweet.user.name}</h5>
         </div>
-        <h3 class="profile-name">${tweet.user.handle}</h3>
+        <h5 class="profile-name">${tweet.user.handle}</h5>
       </section>
       <section>
         <div class="create-tweeter-post">
-        <div class="prev-tweets">
-          <h2>${tweet.content.text}</h2></div>
+          <div class="prev-tweets">
+            <h5>${escape(tweet.content.text)}</h5>
+          </div>
           <hr>
         </div>
         <div class="tweet-btn-counter">
@@ -32,29 +39,47 @@ const createTweetElement = (tweet) => {
 
 const renderTweets = (tweetData) => {
   $("#old-tweet").empty();
-  tweetData.forEach((tweet) => {
-    $("#old-tweet").append(createTweetElement(tweet));
-  })
+  for (let i = tweetData.length - 1; i >= 0; i-- ) {
+    const tweetText = createTweetElement(tweetData[i]);
+    $("#old-tweet").append(tweetText);
+  }
 };
 
 $(document).ready(() => {
-  fetchTweets();
+  loadTweets();
+
   $("#tweet-form").submit((event) => {
     event.preventDefault();
-
-    const tweetData = $(this).serialize();
-    $.ajax({
-      type: "POST",
-      url: "http://localhost:8080/tweets/",
-      data: tweetData,
-      success: () => {
-        fetchTweets();
-      },
-    });
+    const tweetPost = document.forms[0][0].value;
+    const $alert = $('.alert-text');
+    if (tweetPost.length > 140) {
+      return $alert.html(`× Oops! Tweets cannot be more than 140 characters ×`);
+    } else if (tweetPost.length === 0 || tweetPost === null) {
+      return $alert.html(`× Oops! Tweets cannot be empty ×`);
+    } else {
+      const serializedText = $("#tweet-form").serialize();
+      $.post('/tweets', serializedText, (response) => {
+        $.ajax({
+          url: "/tweets/",
+          method: "POST",
+          data: serializedText,
+        })
+        .then(() => {
+          loadTweets();
+          $('.new-tweet').hide();
+          $("#tweet-text").val("");
+          $("output").text();
+        });
+      });
+    };
   });
+  $('.new-tweet').hide();
+  $("#show-hide").on('click', () => {
+      $('.new-tweet').toggle('slow');
+      $("#tweet-form").focus();     
+  })  
 });
-
-const fetchTweets = () => {
+const loadTweets = () => {
   $.ajax({
     type: "GET",
     url: "http://localhost:8080/tweets/",
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -8,23 +8,10 @@ $(document).ready(() => {
     $count.val(140 - $tweetPost.val().length);
     if ($tweetPost.val().length > 140) {
       $('.counter').css("color", "red");     //changes the color of the counter
-      $alert.html("Sorry, we cannot take anymore. Please keep your thoughts within 140 characters");  //show the alert
+      $alert.html(`× Oops! Tweets cannot be more than 140 characters ×`);  //show the alert
     } else if ($tweetPost.val().length <= 140) {
       $('.counter').css("color", "#545149");      //changes the color of the counter back to its original
       $alert.html("");                        //hides the alert
     }
   });
-
-  // const $button = $(".tweeter-btn");
-  // const newTweet = $tweetPost.val();
-  // $button.on('click', () => {
-  //   // console.log(document.forms[0][0].value);
-  //   // console.log(newTweet);
-  //   // console.log(document.forms[0][0].textLength);
-  //   console.log($count.val() - $tweetPost.val().length);
-  //   $tweetPost.val("");
-  // });
-
-  // console.log("Is it working?")
-
 });
