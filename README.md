9ce9e42d1f1ee0ef77972cf14fbfb877d9569f1b
Md Hefzul Bari <ratul14april@gmail.com>
2021-12-17 16:54:15 -0600

    Added scroll to top button, fixed bugs & Added screenshots

--- README.md
+++ README.md
@@ -16,3 +16,18 @@ This repository is the starter code for the project: Students will fork and clon
 
 - Express
 - Node 5.10.x or above
+
+## Features
+1. Create Tweet;
+2. View previous tweets in the timeline in chronological order;
+3. Responsive design.
+
+## How to use the app
+Q. How to create a tweet?
+A. Click the "Write a new Tweet" button on the Navbar. The create new tweet text field will show up. Then write your tweet. Then click the "Tweet" button beneath the post and your tweet will be posted in the timeline. If you want to create another tweet, repeat the same steps.
+
+Q. How long a tweet can be?
+A. The tweet must be between 1 and 140 characters long. Any tweet above 140 characters or less than 1 character cannot be posted and thus show relavant error message. There is a character counter under the right edge of the tweet type area to help the users to determine their tweet length.
+
+Q. Is the app has device compatibility?
+A. The App is designed responsively. So can be used smoothly in any device with a minimum screen width of 320px.
--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,51 +1,61 @@
-const escape = (str) => {
-  let div = document.createElement("h2");
-  div.appendChild(document.createTextNode(str));
-  return div.innerHTML;
-};
-
-const createTweetElement = (tweet) => {
-  const newTweet = $("#old-tweet").append(`
-    <div class="previous-tweets">
-      <section class="profile-identifier">
-        <div class="avatar-fullname">
-          <img class="avatar" src="${tweet.user.avatars}">
-          <h5 class="name">${tweet.user.name}</h5>
-        </div>
-        <h5 class="profile-name">${tweet.user.handle}</h5>
-      </section>
-      <section>
-        <div class="create-tweeter-post">
-          <div class="prev-tweets">
-            <h5>${escape(tweet.content.text)}</h5>
-          </div>
-          <hr>
-        </div>
-        <div class="tweet-btn-counter">
-          <span class="time-till-tweet">
-            <p>${timeago.format(tweet.created_at)}</p>
-          </span>
-          <span class="react-icons">
-            <i class="fas fa-flag"></i>
-            <i class="fas fa-retweet"></i>
-            <i class="fas fa-heart"></i>
-          </span>
-        </div>
-      </section>
-    </div>      
-  `);
-  return newTweet;
-};
-
-const renderTweets = (tweetData) => {
-  $("#old-tweet").empty();
-  for (let i = tweetData.length - 1; i >= 0; i-- ) {
-    const tweetText = createTweetElement(tweetData[i]);
-    $("#old-tweet").prepend(tweetText);
-  }
-};
-
 $(document).ready(() => {
+  const escape = (str) => {
+    let div = document.createElement("h2");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
+
+  const createTweetElement = (tweet) => {
+    const newTweet = $("#old-tweet").append(`
+      <div class="previous-tweets">
+        <section class="profile-identifier">
+          <div class="avatar-fullname">
+            <img class="avatar" src="${tweet.user.avatars}">
+            <h5 class="name">${tweet.user.name}</h5>
+          </div>
+          <h5 class="profile-name">${tweet.user.handle}</h5>
+        </section>
+        <section>
+          <div class="create-tweeter-post">
+            <div class="prev-tweets">
+              <h5>${escape(tweet.content.text)}</h5>
+            </div>
+            <hr>
+          </div>
+          <div class="tweet-btn-counter">
+            <span class="time-till-tweet">
+              <p>${timeago.format(tweet.created_at)}</p>
+            </span>
+            <span class="react-icons">
+              <i class="fas fa-flag"></i>
+              <i class="fas fa-retweet"></i>
+              <i class="fas fa-heart"></i>
+            </span>
+          </div>
+        </section>
+      </div>      
+    `);
+    return newTweet;
+  };
+
+  const renderTweets = (tweetData) => {
+    $("#old-tweet").empty();
+    for (let i = tweetData.length - 1; i >= 0; i--) {
+      const tweetText = createTweetElement(tweetData[i]);
+      $("#old-tweet").prepend(tweetText);
+    }
+  };
+
+  const loadTweets = () => {
+    $.ajax({
+      type: "GET",
+      url: "http://localhost:8080/tweets/",
+      success: (data) => {
+        renderTweets(data);
+      },
+    });
+  };
+
   loadTweets();
 
   $("#tweet-form").submit((event) => {
@@ -58,40 +68,33 @@ $(document).ready(() => {
       return $alert.html(`× Oops! Tweets cannot be empty ×`);
     } else {
       const serializedText = $("#tweet-form").serialize();
-      $.post('/tweets', serializedText, (response) => {   
-          loadTweets();
-          $('.new-tweet').hide();
-          $("#tweet-text").val("");
-          $("output").text();
+      $.post('/tweets', serializedText, (response) => {
+        loadTweets();
+        $('.new-tweet').hide();
+        $("#tweet-text").val("");
+        $(".counter").text("140");
       });
     };
   });
   $('.new-tweet').hide();
   $("#show-hide").on('click', () => {
-      $('.new-tweet').toggle('slow', () => {
-        $("#tweet-text").focus();
-      });
-          
-  })  
-});
-const loadTweets = () => {
-  $.ajax({
-    type: "GET",
-    url: "http://localhost:8080/tweets/",
-    success: (data) => {
-      renderTweets(data);
-    },
-  });
-};
-//Scroll-to-top button
-$(window).scroll(() => {
-  if ($(this).scrollTop()) {
-      $('#to-top-btn').fadeIn();
-  } else {
-      $('#to-top-btn').fadeOut();
-  }
-});
+    $('.new-tweet').toggle('slow', () => {
+      $("#tweet-text").focus();
+    });
 
-$("#to-top-btn").click(() => {
-  $(this).animate({scrollTop: 0}, 1000);
+  })
+
+  //Scroll-to-top button
+  $(window).scroll(() => {
+    if ($(this).scrollTop()) {
+      $('#to-top-btn').fadeIn();
+    } else {
+      $('#to-top-btn').fadeOut();
+    }
+  });
+
+  $("#to-top-btn").on('click', () => {
+    console.log('Clicked');
+    $(window).scrollTop(0);
+  });
 });
